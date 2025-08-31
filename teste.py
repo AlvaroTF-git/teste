de6aa6b2ebdafe16412d47d58729c538bd7ae454
AlvaroTF-git <alvaro.tferreir4@gmail.com>
2025-08-31 14:03:18 +0000

    Definitivo

--- teste.py
+++ teste.py
@@ -4,18 +4,26 @@ def contador(inicio=0, fim=0, passo=0):
     print('-=' * 20)
     print(f'contagem de {inicio} até {fim}, com passo de {passo}: ')
     print('-=' * 20)
+    if passo < 0:
+        passo *= -1
     while True:
         ##contador decrescente
         if inicio >= fim:
+            if passo == 0:
+                print("Erro, seu passo não pode ser 0!")
+                break
             sleep(0.5)
-            print(f"{inicio} |", end=' ')
+            print(f"{inicio} |", end=' ', flush=True)
             inicio -= passo
             if inicio < fim:
                 break
         ##contador crescente
         elif inicio <= fim:
+            if passo == 0:
+                print("Erro, seu passo não pode ser 0!")
+                break
             sleep(0.5)
-            print(f"{inicio} |", end=' ')
+            print(f"{inicio} |", end=' ', flush=True) ##flush evita buffer, permitindo utilização correta do (sleep)
             inicio += passo
             if inicio > fim:
                 break 
@@ -29,5 +37,5 @@ print('Contador!')
 contador(0, 10, 2)
 contador(10, 0, 2)
 print()
-print('Agora é sua vez de personalizar a contagem!')    
-contador(int(input("inicio: ")), int(input("fim: ")), int(input("passo: ")))
+print("Agora é sua vez de personalizar a contagem :") 
+contador(int(input("inicio--> ")), int(input("fim--> ")), int(input("passo--> ")))
